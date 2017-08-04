f2d336e59d79737988ad1652fbc53f8d490d65b3
IamDrowsy <mail@drowsy.de>
2017-08-04 23:19:54 +0200

    changed back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ebenbild "0.1.0"
+(defproject ebenbild "0.1.0-SNAPSHOT"
   :description "tiny library to create predicates from different data that match on \"look-a-likes\""
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
