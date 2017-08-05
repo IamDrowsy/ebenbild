bbc10edaa178c7f7a621119a8d0f0182d97b229f
IamDrowsy <mail@drowsy.de>
2017-08-05 13:48:41 +0200

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ebenbild "0.1.0"
+(defproject ebenbild "0.1.1-SNAPSHOT"
   :description "tiny library to create predicates from different data that match on \"look-a-likes\""
   :url "https://github.com/IamDrowsy/ebenbild"
   :license {:name "Eclipse Public License"
