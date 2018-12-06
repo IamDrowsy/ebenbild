30a941e3f9b6902b82b25f3120171f8dfccb34bc
Albrecht Schmidt <mail@albrechtschmidt.de>
2018-12-06 22:47:45 +0100

    to next snapshot and circle ci file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ebenbild "0.1.1"
+(defproject ebenbild "0.3.0-SNAPSHOT"
   :description "tiny library to create predicates from different data that match on \"look-a-likes\""
   :url "https://github.com/IamDrowsy/ebenbild"
   :license {:name "Eclipse Public License"
