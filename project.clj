d16af2f9b492849ef64eef6a94dcb5d5416148c5
Albrecht Schmidt <mail@albrechtschmidt.de>
2019-07-01 21:40:42 +0200

    dependency updates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ebenbild "0.2.0"
+(defproject ebenbild "0.3.0"
   :description "tiny library to create predicates from different data that match on \"look-a-likes\""
   :url "https://github.com/IamDrowsy/ebenbild"
   :license {:name "Eclipse Public License"
@@ -11,9 +11,9 @@
                                    :target :nodejs
                                    :optimizations :none}}}
               :test-commands {"tests" ["node" "target/test.js"]}}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [org.clojure/test.check "0.9.0"]
-                                  [org.clojure/clojurescript "1.10.439"]
-                                  [com.cemerick/piggieback "0.2.2"]
-                                  [olical/cljs-test-runner "3.2.0"]]
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
+                                  [org.clojure/clojurescript "1.10.520"]
+                                  [cider/piggieback "0.4.1"]
+                                  [olical/cljs-test-runner "3.7.0"]]
+                   :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}})
